471782a22fd514bfe70f86f6a739877c000d8504
nascott <nascott94@outlook.com>
2021-08-29 19:50:54 -0600

    data persistent only on local side

--- Develop/server.js
+++ Develop/server.js
@@ -29,10 +29,24 @@ app.get("/notes", (req, res) =>
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
 
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
+
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
 
 const readAndAppend = (content, file) => {
   fs.readFile(file, "utf8", (err, data) => {
@@ -53,7 +67,7 @@ app.get("/api/db", (req, res) => {
 });
 
 // POST Route for a note
-app.post("/api/db", (req, res) => {
+app.post("/api/notes", (req, res) => {
   console.info(`${req.method} request received to add a note`);
 
   const { title, text } = req.body;
